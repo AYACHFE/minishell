3b512eb3c1e28d6bc9ead5cb70b8fd9b174c8eb8
Aymane Achfennar <aachfenn@c2r1p1.1337.ma>
2023-05-28 13:14:16 +0100

    multiple cmds

--- execution/exec_1.c
+++ execution/exec_1.c
@@ -6,7 +6,7 @@
 /*   By: aachfenn <aachfenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 13:50:59 by aachfenn          #+#    #+#             */
-/*   Updated: 2023/05/27 20:30:17 by aachfenn         ###   ########.fr       */
+/*   Updated: 2023/05/28 13:10:34 by aachfenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,51 +16,44 @@ void	exec_1(t_minishell	*mini, t_cmd	*cmd, char	**env)
 {
 	int	i;
 	int	fd[2];
-	// int	fd_2[2];
 	int	pid;
-	// int	last_read;
+	int	stdi;
 
 	i = 0;
 	(void)cmd;
 	(void)mini;
 	(void)env;
-	if (cmd->general_info->cmd_nb == 1){
-		built_in_cmd_fork(mini, &cmd[i], env);
-		return ;
-	}
-	// while (i < cmd->general_info->cmd_nb)
-	// {
+	stdi = dup(0);
+	while (i < cmd->general_info->cmd_nb - 1)
+	{
 		if (pipe(fd) == -1)
 			exit(0);
-		// if (pipe(fd_2) == -1)
-		// 	exit(0);
-			// last_read = fd[0];
 		pid = fork();
 		if (pid == 0)
 		{
 			dup2(fd[1], 1);
 			close(fd[1]);
 			close(fd[0]);
-			// close(0);
-			
 			built_in_cmd_2(mini, &cmd[i], env);
-			// exit(0);
 		}
-		i++;
-		pid = fork();
-		if (pid == 0)
-		{
-			dup2(fd[0], 0);
-			close(fd[0]);
-			// close(fd[0]);
-			close(fd[1]);
-			// close(1);
-			built_in_cmd_2(mini, &cmd[i], env);
-			// puts("\n\nhere--->\n\n");
-		}
+		dup2(fd[0], 0);
 		close(fd[1]);
 		close(fd[0]);
-			// wait(NULL);
-	// 	i++;
-	// }
+		i++;
+	}
+	if (pipe(fd) == -1)
+		exit(0);
+	pid = fork();
+	if (pid == 0)
+	{
+		
+		close(fd[0]);
+		close(fd[1]);
+		built_in_cmd_2(mini, &cmd[i], env);
+	}
+	dup2(stdi, 0);
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid, NULL, 0);
+	while (wait(NULL) > 0) ;
 }
--- src/built_in.c
+++ src/built_in.c
@@ -6,7 +6,7 @@
 /*   By: aachfenn <aachfenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:45:13 by aachfenn          #+#    #+#             */
-/*   Updated: 2023/05/27 20:29:28 by aachfenn         ###   ########.fr       */
+/*   Updated: 2023/05/28 13:13:21 by aachfenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,13 +41,13 @@ void	built_in_cmd(t_minishell	*mini, char **env)
 	mini->count_str = count(str, ' ');
 	if (ft_error(str) == 0)
 		return ;
-	parcing(mini, cmd, str);
 	ft_check_dollar(mini);
+	parcing(mini, cmd, str);
 
 	exec_1(mini, cmd, env);
 
 	add_history(str);
-	wait(NULL);
+	// wait(NULL);
 	free(str);
 }	
 
@@ -80,55 +80,10 @@ void	execv_function(t_minishell	*mini, t_cmd	*cmd, char **env)
 		val = ft_strjoin(val, cmd->args[0]);
 		if (access(val, F_OK) == 0)
 		{
-			execve(val, cmd->args, env);
 			free(var[j]);
 			free(var);
-			free(val);
+			execve(val, cmd->args, env);
 			exit(0);
-		}
-		free(var[j]);
-		free(val);
-		j++;
-	}
-	free(var);
-}
-
-void	execv_function_fork(t_minishell	*mini, t_cmd	*cmd, char **env)
-{
-	(void)mini;
-	int		i;
-	int		j;
-	int		path_nb;
-	char	**var = NULL;
-	char	*val;
-
-
-	i = 0;
-	j = 0;
-	while (env[i] != NULL)
-	{
-		if (ft_strncmp(env[i], "PATH", 4) == 0)
-		{
-			var = ft_split(ft_substr(env[i], 5, ft_strlen(env[i])), ':');
-			break ;
-		}
-		i++;
-	}
-	path_nb = count(ft_substr(env[i], 5, ft_strlen(env[i])), ':');
-	while (j < path_nb)
-	{
-		val = ft_strjoin(var[j], "/");
-		val = ft_strjoin(val, cmd->args[0]);
-		if (access(val, F_OK) == 0)
-		{
-			if (fork() == 0)
-			{
-				execve(val, cmd->args, env);
-				free(var[j]);
-				free(var);
-				free(val);
-				exit(0);
-			}
 		}
 		free(var[j]);
 		free(val);
@@ -156,27 +111,6 @@ void	built_in_cmd_2(t_minishell	*mini, t_cmd	*cmd, char **env)
 		ft_export(mini);
 	else
 		execv_function(mini, cmd, env);
-}
-
-void	built_in_cmd_fork(t_minishell	*mini, t_cmd	*cmd, char **env)
-{
-	(void)cmd;
-	if (ft_strncmp(mini->str[0], "exit", ft_strlen(mini->str[0])) == 0)
-		exit(42);
-	else if (ft_strncmp(mini->str[0], "echo", ft_strlen(mini->str[0])) == 0)
-		ft_echo(mini);
-	else if (ft_strncmp(mini->str[0], "cd", ft_strlen(mini->str[0])) == 0)
-		ft_cd(mini);
-	else if (ft_strncmp(mini->str[0], "pwd", ft_strlen(mini->str[0])) == 0)
-		ft_pwd();
-	else if (ft_strncmp(mini->str[0], "env", ft_strlen(mini->str[0])) == 0)
-		ft_env(env, mini);
-	else if (ft_strncmp(mini->str[0], "unset", ft_strlen(mini->str[0])) == 0)
-		ft_unset(mini);
-	else if (ft_strncmp(mini->str[0], "export", ft_strlen(mini->str[0])) == 0)
-		ft_export(mini);
-	else
-		execv_function_fork(mini, cmd, env);
 }
 
 int	ft_cd(t_minishell	*mini)
